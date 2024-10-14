41755b3c7d28945d5985345cd6b4beeada82041b
SuvitKumar003 <sksuvit2003@gmail.com>
2024-10-14 18:08:28 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+#from typing import list
+setup(
+  name='sensor_detection',
+  version="0.0.1",
+  author="suvit",
+  author_email="sksuvit2003@gmail.com",
+  packages=find_packages(),
+  
+)
