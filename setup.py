110948a2c28426d3e57c1e1898844cc0a3dd81d6
SuvitKumar003 <sksuvit2003@gmail.com>
2024-10-14 18:13:49 +0530

    requirement file updated

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import find_packages,setup
-#from typing import list
+#from typing import List
+
+
 setup(
   name='sensor_detection',
   version="0.0.1",
   author="suvit",
   author_email="sksuvit2003@gmail.com",
   packages=find_packages(),
-  
+  install_requires=["pymongo"]
+
 )
