27ca9736667de59139f6036bc5aefc62e1c2f29c
SuvitKumar003 <sksuvit2003@gmail.com>
2024-10-31 07:04:51 +0530

    COMMIT

---
+++ sensor/configuration/mongo_db_connection.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import pymongo
+from sensor.constants.database import DATABASE_NAME
+import certifi
+ca=certifi.where()
+from sensor.constants.env_variable import MONGO_URL_KEY
+import os
+import logging
+
+load_dotenv()
+
+class MongoDBClient:
+  client=None
+
+  def __init__(self,database_name=DATABASE_NAME)->None:
+
+    try:
+
+      if MongoDBClient.client is None:
+        mongo_db_url=os.getenv(MONGO_URL_KEY)
+        logging.info(" Retrive mongo db url i s:{mongo_db_url}")
+      else:
+        Mong 
